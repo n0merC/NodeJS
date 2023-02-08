ed62a3813321e80bfb64347d0717c30ed546aea5
n0merC <singhsamrat677@gmail.com>
2023-02-08 08:54:17 +0545

    samrat

--- index.js
+++ index.js
@@ -5,10 +5,19 @@ app.get('/get-hello', (req,res)=>{
     res.send('Hello baucha...');
 })
 
+function apple(a,b){
+    const ss = parseInt(a+b);
+    console.log(ss)
+    return ss
+}
 app.post('/get-hillo', (req,res)=>{
-    let c = parseInt(req.query.a)+parseInt(req.query.b);
-
-    res.status(200).send('sum = '+c)
+    
+    // let c = parseInt(req.query.a)+parseInt(req.query.b);
+    // res.status(200).send('sum = '+c)
+    let a = req.body.a;
+    let b = req.body.b;
+    let s = apple(a,b);
+    res.status(200).send('sum ='+s);
 })
 
 app.listen(port, () => {
