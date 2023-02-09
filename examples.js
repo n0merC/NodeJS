dc5d812fb7f8b860cca385bc866bfc85524603ea
n0merC <singhsamrat677@gmail.com>
2023-02-09 08:20:18 +0545

    promise

--- examples.js
+++ examples.js
@@ -14,14 +14,24 @@ function getOrderItem(orders){
     console.log("getOrderITems ===", orders)
 }
 
-function addNewItem(getOrderItem){
-    setTimeout(()=>{
-        orders.push({fruits:"apple",qty:4})
-        getOrderItem(orders)
-    },6000);
-    // return getOrderItem()
+function addNewItem(){
+    
+    return new Promise((resolve,reject)=>{
+        const err = false
+        setTimeout(()=>{
+            orders.push({fruits:"apple",qty:4})
+            if(err)
+            resolve()
+            reject();
+        },6000);
+    })
 }
 
 
-addNewItem(getOrderItem);
+addNewItem().then(response =>{
+    console.log("response success ===")
+    getOrderItem(orders)
+}).catch((err)=>{
+    console.log("err ===")
+});
 // getOrderItem(orders);
